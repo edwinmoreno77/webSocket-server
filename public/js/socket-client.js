49df1e5003750ff396ddf2f49126dfb2b796e095
Edwin Moreno <edwinmoreno77@gmail.com>
2022-12-29 12:14:33 -0400

    controllers added

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -10,18 +10,22 @@ const socket = io();
 
 
 socket.on('connect', () => {
-    console.log('Conectado');
+    // console.log('Conectado');
 
     lblOffline.style.display = 'none';
     lblOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-    console.log('Desconectado del servidor');
+    // console.log('Desconectado del servidor');
 
     lblOffline.style.display = '';
     lblOnline.style.display = 'none';
 
+});
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log(payload);
 });
 
 btnEnviar.addEventListener('click', () => {
@@ -32,6 +36,6 @@ btnEnviar.addEventListener('click', () => {
         fecha: new Date().getTime()
     }
     socket.emit('enviar-mensaje', payload, (id) => {
-        console.log('Desde el server', payload);
+        console.log('Desde el server', id);
     });
 });
