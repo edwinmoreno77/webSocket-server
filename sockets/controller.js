fb2003b7e69a527474567df732e350cf9382f8b9
Edwin Moreno <edwinmoreno77@gmail.com>
2022-12-29 18:30:52 -0400

    first version added

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,17 +1,54 @@
 
 
-const socketController = (socket) => {
-    console.log('Cliente conectado', socket.id);
+const TicketControl = require('../models/ticket-control');
 
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id);
+const ticketControl = new TicketControl();
+
+const socketController = (socket) => {
+    //cuando un cliente se conecta
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+    socket.emit('estado-actual', ticketControl.ultimos4);
+    socket.emit('tickets-pendientes', ticketControl.tickets.length)
+
+
+    socket.on('siguiente-ticket', (payload, callback) => {
+
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+
+        //Notificar que hay un nuevo ticket pendiente de asignar
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length)
     });
 
-    socket.on('enviar-mensaje', (payload, callback) => {
-        const id = 123456;
-        callback(id);
+    socket.on('atender-ticket', ({ escritorio }, callback) => {
 
-        socket.broadcast.emit('enviar-mensaje', payload);
+        if (!escritorio) {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket(escritorio);
+
+        //actualizar / notificar cambios en la cola de tickets pendientes
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length)
+        socket.emit('tickets-pendientes', ticketControl.tickets.length)
+
+        //TODO: Notificar cambios en los últimos 4
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+
+        if (!ticket) {
+            return callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
     });
 }
 
